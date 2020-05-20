ceced0ec140eb7b817f14d46d61322b09a7ea68f
Lenasim <radical.rin@gmail.com>
2020-05-20 20:11:11 +0200

    login password security added

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,24 @@ const userSchema = mongoose.Schema({
         type: Number,
     },
 })
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        //비밀번호암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        });
+    } else {
+        next()
+    } 
+});
 
 const User = mongoose.model('User', userSchema)
 
