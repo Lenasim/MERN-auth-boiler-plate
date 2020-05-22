0550c29530a02d61032d6a2c91ed8776b3fe804e
Lenasim <radical.rin@gmail.com>
2020-05-22 23:22:55 +0200

    logout setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,16 +2,23 @@ import React, {useEffect} from 'react'
 import axios from 'axios'
 import './LandingPage.css'
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(res => console.log(res))
     },[])
 
+    const logoutHandler = () => {
+        axios.get('/api/users/logout')
+        .then(res => res.data.success? props.history.push('/login'): alert("Failed to log out"))
+    }
+
     return (
         <div className="landingpage">
            <h2>Landing Page</h2> 
+
+           <button onClick={logoutHandler}>Log out</button>
         </div>
     )
 }
